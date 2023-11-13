ccefa124c5ee8347e86de6b1a775b822914cc5a2
Gürkan <gurkanyigitgr@gmail.com>
2023-11-13 23:40:00 +0300

    emailSection &apos;

--- components/main/EmailSection.tsx
+++ components/main/EmailSection.tsx
@@ -40,8 +40,8 @@ const EmailSection = () => {
         <p className='text-[#ADB7BE] mb-4 max-w-md'>
           {' '}
           I&apos;m currently looking for new opportunities, my inbox is always
-          open. Whether you have a question or just want to say hi, I'll try my
-          best to get back to you!
+          open. Whether you have a question or just want to say hi, I&apos;ll
+          try my best to get back to you!
         </p>
         <div className='socials flex flex-row gap-2'>
           <a href='https://github.com/gurkanyigitgr' target='_blank'>
