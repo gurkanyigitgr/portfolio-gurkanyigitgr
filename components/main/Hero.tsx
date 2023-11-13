8819a9265b28fe1c9f49473cc09db22fca90b1d7
Gürkan <gurkanyigitgr@gmail.com>
2023-11-14 00:10:08 +0300

    add playsInline for hero section's video

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -6,6 +6,7 @@ export default function Hero() {
     <div className='relative flex flex-col h-full w-full'>
       <video
         autoPlay
+        playsInline
         muted
         loop
         className='rotate-180 absolute top-[-500px] md:top-[300px] left-0 z-[1] w-full h-full object-cover sm:object-contain'
