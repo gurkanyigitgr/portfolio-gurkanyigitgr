f4820b46fdbd3c99e895dcc14ac09354fd4fd119
Gürkan <gurkanyigitgr@gmail.com>
2023-11-14 00:49:59 +0300

    change type for video

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
         loop
         className='rotate-180 absolute top-[-500px] md:top-[300px] left-0 z-[1] w-full h-full object-cover sm:object-contain'
       >
-        <source src='/blackhole.mp4' type='video/webm' />
+        <source src='/blackhole.mp4' type='video/mp4' />
       </video>
       <HeroContent />
     </div>
